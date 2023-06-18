6d07bd609c79f35a579f18c32cd095a72ad33fea
Khaled Allam <kaaoaa505@gmail.com>
2023-06-18 09:40:08 +0300

    Generate calc proto and use sum method Client set first number and second number Server response with sum result

--- client/client.js
+++ client/client.js
@@ -3,8 +3,16 @@ var grpc = require('@grpc/grpc-js');
 var greet_pb = require('../server/proto/greet_pb');
 var greet_grpc_pb = require('../server/proto/greet_grpc_pb');
 
-function main(){    
+var calc_pb = require('../server/proto/calc_pb');
+var calc_grpc_pb = require('../server/proto/calc_grpc_pb');
+
+function main(){
     var greet_client = new greet_grpc_pb.GreetServiceClient(
+        'localhost:50051',
+        grpc.credentials.createInsecure()
+    );
+
+    var calc_client = new calc_grpc_pb.calcServiceClient(
         'localhost:50051',
         grpc.credentials.createInsecure()
     );
@@ -16,6 +24,10 @@ function main(){
     var greet_request = new greet_pb.GreetRequest();
     greet_request.setGreeting(greetObj);
 
+    var calc_request = new calc_pb.SumRequest();
+    calc_request.setFirstNumber(3);
+    calc_request.setSecondNumber(4);
+
     greet_client.greet(greet_request, (error, response)=>{
         if(!error){
             console.log('✅ Greeting Response is: ', response.getResult());
@@ -23,5 +35,13 @@ function main(){
             console.log('✅ Greeting Response -ERROR- is: ', error);
         }
     });
+
+    calc_client.sum(calc_request, (error, response) => {
+        if(!error){
+            console.log('✅ Sum Response is: ', response.getSumResult());
+        }else{
+            console.log('✅ Sum Response -ERROR- is: ', error);
+        }
+    })
 }
 main();
--- server/server.js
+++ server/server.js
@@ -1,25 +1,37 @@
 var grpc = require('@grpc/grpc-js');
 
-var greets = require('./proto/greet_pb');
-var service = require('./proto/greet_grpc_pb');
+var greet_pb = require('./proto/greet_pb');
+var greet_grpc_pb = require('./proto/greet_grpc_pb');
+
+var calc_pb = require('./proto/calc_pb');
+var calc_grpc_pb = require('./proto/calc_grpc_pb');
 
 function greet(call, callback){
-    var greeting = new greets.GreetResponse();
+    var response = new greet_pb.GreetResponse();
 
-    greeting.setResult(
+    response.setResult(
         'Hello, ' + 
         call.request.getGreeting().getFirstName() +
         ' ' +
         call.request.getGreeting().getLastName()
     );
 
-    callback(null, greeting);
+    callback(null, response);
+}
+
+function sum(call, callback){
+    var response = new calc_pb.SumResponse();
+   
+    response.setSumResult(call.request.getFirstNumber() + call.request.getSecondNumber());
+
+    callback(null, response);
 }
 
 function main(){
     var server = new grpc.Server();
 
-    server.addService(service.GreetServiceService, {greet: greet});
+    server.addService(greet_grpc_pb.GreetServiceService, {greet: greet});
+    server.addService(calc_grpc_pb.calcServiceService, {sum: sum});
 
     server.bindAsync('127.0.0.1:50051', grpc.ServerCredentials.createInsecure(), ()=>{
         server.start();
